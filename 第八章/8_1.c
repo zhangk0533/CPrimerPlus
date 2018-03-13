ba2da17594579a35bbd5069c374029ba6340b6ac
zhangk0533 <907144187@qq.com>
2018-03-13 11:40:39 +0800

    1

---
+++ 第八章/8_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	int num = 0;
+	char ch;
+	while(ch=getchar()!='\n')
+	{
+		num++;
+	}
+	printf("×Ö·û¸öÊýÎª %d",num); 
+	
+	return 0;
+}
